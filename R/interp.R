c0ca395380a3f675a255b6acc14aa5138856c828
brodieG <brodieG@users.noreply.github.com>
2020-06-13 13:56:48 -0400

    interp 'working'
    I.e. there is output, but it appears to be garbage.

--- R/interp.R
+++ R/interp.R
@@ -5,6 +5,7 @@
 #'
 #' @export
 #' @importFrom gridBezier BezierGrob BezierPoints nSteps
+#' @importFrom grid unit convertX convertY
 #' @seealso [parse_path()]
 #' @param x a "subpath" S3 objects as produced by [parse_path()]
 #' @param width numeric(1) or numeric(2) or NULL, the width of the device, used
@@ -16,12 +17,22 @@
 #'   The bounding box is defined simply to contain the supplied points,
 #'   including control points.
 #' @param height like `width`, except for the y range.
+#' @param normalize whether coordinates should be returned in 0-1 range.
 #' @return a "subpath_xy" S3 object, which is just like a "subpath" object, but
 #'   the coordinates are expressed purely as x-y values and should be
 #'   interpreted as the vertices of a polygon or connected straight line
 #'   segments.
 
-interp_path <- function(x, steps=10, width=NULL, height=NULL) {
+interp_path <- function(
+  x, steps=10, width=NULL, height=NULL, normalize=FALSE
+) {
+  vetr(
+    structure(list(), class='subpath'),
+    INT.1.POS,
+    NULL || integer(1L) || integer(2L),
+    NULL || integer(1L) || integer(2L),
+    LGL.1
+  )
   d <- x[['d']]
   if(is.null(width)) width <- range(d[['x']])
   if(is.null(height)) height <- range(d[['y']])
@@ -39,27 +50,27 @@ interp_path <- function(x, steps=10, width=NULL, height=NULL) {
   bends <- with(brle, cumsum(lengths)[values])
   bstarts <- with(brle, cumsum(lengths)[!values])
 
+  # BezierGrob seems to open a display device...
+
   bzs <- Map(
     function(start, end) {
       points <- d[start:end, c('x', 'y')]
       BezierGrob(points[[1]], points[[2]], stepFn=nSteps(steps))
     },
-    bstarts,
+    bstarts[seq_along(bends)],
     bends
   )
   # Can't figure out how to get BezierPoints to not return in inches, so
-  # converting back to NPC manually, which is a real hack.  Need to make sure
-  # `par$pin()` is properly set to use, so we init device. (Is this still true?)
+  # converting back to NPC manually, which is a real hack.
 
   bzsp <- lapply(bzs, BezierPoints)
   bzspi <- lapply(bzsp, lapply, unit, 'inches')
   bzsp <- lapply(
     bzspi, function(x) list(convertX(x[[1]], 'npc'), convertY(x[[2]], 'npc'))
   )
-  # plot.new(); lapply(bzs, grid.draw)
-  # reconnect with the line segments
+  # connect with the line segments
 
-  lrle <- with(dr, rle(cmd == "L"))
+  lrle <- with(d, rle(cmd == "L"))
   lends <- with(lrle, cumsum(lengths)[values])
   lstarts <- with(lrle, cumsum(lengths)[!values][seq_along(lends)])
 
@@ -70,10 +81,19 @@ interp_path <- function(x, steps=10, width=NULL, height=NULL) {
   )
   # Recombine
 
-  # lapply(
-  #   lnsp,
-  #   function(x) grid.lines(x[[1]], x[[2]], gp=gpar(col='#000000'))
-  # )
+  els <- c(bzsp, lnsp)[order(c(bstarts, lstarts))]
+  xvals <- unlist(lapply(els, '[[', 1))
+  yvals <- unlist(lapply(els, '[[', 2))
+
+  # Rescale
+
+  if(!normalize) {
+    xvals <- (xvals * diff(width)) + width[1]
+    yvals <- (yvals * diff(height)) + height[1]
+  }
+  class(x) <- "subpath_xy"
+  x[['d']] <- data.frame(x=xvals, y=yvals)
+  x
 }
 
 
