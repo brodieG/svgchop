d2ff8e33556f23ec088c3c2e308f82a44875c748
brodieG <brodieG@users.noreply.github.com>
2020-10-09 19:12:07 -0400

    remove fuse code

--- R/arc.R
+++ R/arc.R
@@ -23,20 +23,6 @@
 #
 # Some parameter names survive from the fuselibs implementation.
 
-svg_angle <- function(ux, uy, vx, vy ) {
-  u <- c(X=ux, Y=uy);
-  v <- c(X=vx, Y=vy);
-  # (F.6.5.4)
-  dot <- sum(u * v)
-  # # Not sure this one is translated correctly
-  # len <- Vector.Length(u) * Vector.Length(v);
-  len <- sqrt(sum(u^2)) * sqrt(sum(v^2))
-  #floating point precision, slightly over values appear
-  ang <- acos(max(min(dot / len,1),-1))
-  if ( (u[1L]*u[2L] - u[2L]*u[1L]) < 0)
-          ang <- -ang;
-  ang
-};
 ## Convert SVG Arc Coordinates to Center Point Based
 ##
 ## Perform the endpoint to center arc parameter conversion as detailed in
@@ -54,66 +40,6 @@ svg_angle <- function(ux, uy, vx, vy ) {
 ## * "ang" `numeric(2)`, the theta (from +ve x-axis, clockwise
 ##    presumably, actually not obvious as standard rot matrix rotates
 ##    counter-clockwise) and delta angles in degrees.
-
-arc_endpoint_to_center <- function(
-  p1, p2, r, xAngle, flagA, flagS
-)
-{
-  rX <- abs(r[1L])
-  rY <- abs(r[2L])
-  xAngle <- xAngle / 180 * pi
-
-  #(F.6.5.1) re-center and rotate
-  dx2 <- (p1[1L] - p2[1L]) / 2.0;
-  dy2 <- (p1[2L] - p2[2L]) / 2.0;
-  x1p <- cos(xAngle)*dx2 + sin(xAngle)*dy2;
-  y1p <- -sin(xAngle)*dx2 + cos(xAngle)*dy2;
-
-  #(F.6.5.2)
-  rxs <- rX * rX;
-  rys <- rY * rY;
-  x1ps <- x1p * x1p;
-  y1ps <- y1p * y1p;
-
-  # check if the radius is too small `pq < 0`, when `dq > rxs * rys` (see below)
-  # cr is the ratio (dq : rxs * rys)
-  cr <- x1ps/rxs + y1ps/rys;
-  if (cr > 1) {
-    #scale up rX,rY equally so cr == 1
-    s <- sqrt(cr);
-    rX <- s * rX;
-    rY <- s * rY;
-    rxs <- rX * rX;
-    rys <- rY * rY;
-  }
-  dq <- (rxs * y1ps + rys * x1ps);
-  pq <- (rxs*rys - dq) / dq;
-  q <- sqrt(max(0,pq)) #use Max to account for float precision
-  if (flagA == flagS)
-    q <- -q;
-  cxp <- q * rX * y1p / rY;
-  cyp <- - q * rY * x1p / rX;
-
-  #(F.6.5.3)
-  cx <- cos(xAngle)*cxp - sin(xAngle)*cyp + (p1[1L] + p2[1L])/2
-  cy <- sin(xAngle)*cxp + cos(xAngle)*cyp + (p1[2L] + p2[2L])/2
-
-  #(F.6.5.5)
-  theta <- svg_angle(1, 0, (x1p-cxp) / rX, (y1p - cyp)/rY )
-  #(F.6.5.6)
-  delta <- svg_angle(
-    (x1p - cxp)/rX, (y1p - cyp)/rY,
-    (-x1p - cxp)/rX, (-y1p - cyp)/rY
-  )
-  delta <- delta %% (2 * pi);
-  if (!flagS && delta > 0) delta <- delta - 2 * pi;
-
-  r <- c(rX, rY)
-  c <- c(cx, cy)
-  angles <- c(theta, delta)
-  list(r=r, c=c, ang=angles / pi * 180)
-}
-
 
 vec_ang <- function(u, v) {
   det <- sign(u[1] * v[2] - u[2] * v[1])
