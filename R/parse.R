10861b8e27a14d404ca17489598ab779f112a963
brodieG <brodieG@users.noreply.github.com>
2020-10-11 15:41:16 -0400

    implement rect, begin transform implement

--- R/parse.R
+++ R/parse.R
@@ -259,7 +259,7 @@ path_simplify <- function(path, steps) {
 ##   `c("M","L","C")`.
 
 parse_d <- function(x, steps) {
-  if(!is.character(x) || length(x) != 1) stop("Input not character(1L)")
+  vetr(character(1L))
   raw <- regmatches(x, gregexpr("-?[0-9]*\\.?[0-9]+|[a-zA-Z]", x))[[1]]
   raw <- unname(split(raw, cumsum(grepl("[a-zA-Z]", raw))))
   cmds <- lapply(
@@ -303,8 +303,7 @@ parse_path <- function(x, steps=5) {
     )
   x
 }
-#' @rdname parse_path
-#' @export
+## @param x named character vector with properties attached to a polygon
 
 parse_poly <- function(x) {
   x <- as.list(x)
@@ -325,6 +324,92 @@ parse_poly <- function(x) {
   }
   x[['coords']] <- list(structure(coords, class=c('subpath','data.frame')))
   x
+}
+## @inheritParams parse_poly
+
+parse_rect <- function(x) {
+  props <- names(x)
+  if(any(c('rx', 'ry', 'pathLength') %in% props))
+    warning('"r[xy] and pathLength properties on "rect" not supported')
+
+  if(!'x' %in% props) x['x'] <- "0"
+  if(!'y' %in% props) x['y'] <- "0"
+  if(!all(c('width', 'height') %in% props))
+    stop('"rect" requires width and height specified')
+
+  base.props <- c('x', 'y', 'width', 'height')
+  coords <- setNames(as.numeric(x[base.props]), base.props)
+  if(anyNA(coords))
+    stop('"rect" can only be processed if all base properties are numeric')
+
+  xs <- c(
+    coords['x'], coords['x'] + coords['width'],
+    coords['x'] + coords['width'], coords['x']
+  )
+  ys <- c(
+    coords['y'], coords['y'],
+    coords['y'] + coords['height'], coords['y'] + coords['height']
+  )
+  p <- paste(xs, ys, sep=",", collapse=" ")
+  parse_poly(c(points=p, x[!props %in% base.props]))
+}
+
+## @param x a list maybe containing 'transform' and 'coords' elements
+
+parse_transform <- function(x, CMS=diag(3)) {
+  raw <- [[1L]]
+  cstart <- attr
+
+  raw <- regmatches(x, gregexpr("-?[0-9]*\\.?[0-9]+|[a-zA-Z]", x))[[1]]
+
+  if('transform' %in% names(x) && 'coords' %in% names(x)) {
+    trans <- x['transform']
+    vet(character(1L), trans)
+    raw <- gregexpr("([a-zA-Z]+)\\s*\\(([^)]*)\\)", trans, perl=TRUE)[[1L]]
+    cs <- attr(raw, 'capture.start')
+    cl <- attr(raw, 'capture.length')
+    proc1 <- substr(rep(x, length(cs)), c(cs), c(cs + cl))
+    cmds <- proc1[seq_len(nrow(cs))]
+    vals <- proc1[seq_len(nrow(cs)) + nrow(cs)]
+
+    vals2 <- lapply(
+      regmatches(x, gregexpr("-?[0-9]*\\.?[0-9]+")), as.numeric
+    )
+    if(any(vapply(vals2, anyNA, TRUE)))
+      stop('unparseable parameters in transform command')
+
+    coords <- as.matrix(x[['coords']][, c('x', 'y')])
+    CMS <- diag(3)
+
+    for(i in seq_along(cmds)) {
+      CMS.tmp <- diag(3)
+      valsi <- as.numeric(vals[[i]])
+      switch(cmds[i],
+        translate={
+          if(lengths(valsi) == 2) {
+            CMS.tmp[1:2,3] <- valsi
+          } else if(lengths(valsi) == 1) {
+            CMS.tmp[1,3] <- valsi
+          } else stop('Invalid "translate" command')
+        },
+        rotate={
+          CMS.tmp <- diag
+          if(!length(valsi) %in% c(1, 3)) stop('Invalid "rotate" command')
+          ang <- valsi[1] / 180 * pi
+
+          CMS.tmp[1:2, 1:2] <- c(cos(ang), sin(ang), -sin(ang), cos(ang))
+          if(length(valsi) == 3) {
+            trans1 <- trans2 <- diag(3)
+            trans1[3, 1:2] <- valsi[2:3]
+            trans2[3, 1:2] <- -valsi[2:3]
+            CMS.tmp <- trans1 %*% CMS.tmp %*% trans2
+          }
+        },
+        stop('"', cmds[i], '" transformation not supported')
+      )
+      CMS <- CMS %*% CMS.tmp
+    }
+  }
 }
 
 #' Retrieve SVG Elements From File
@@ -387,15 +472,17 @@ parse_svg <- function(file) {
   if(is.na(x)) x <- 0
   if(is.na(y)) y <- 0
 
-  els <- xml_find_all(xml, ".//path|.//polygon")
+  els <- xml_find_all(xml, ".//path|.//polygon|.//rect")
   type <- xml_name(els)
   el.path <- type == 'path'
   el.poly <- type == 'polygon'
+  el.rect <- type == 'rect'
 
   res <- list(length(els))
   res[el.path] <- lapply(xml_attrs(els[el.path]), parse_path)
   res[el.poly] <- lapply(xml_attrs(els[el.poly]), parse_poly)
-
+  res[el.rect] <- lapply(xml_attrs(els[el.rect]), parse_rect)
+  res <- parse_transform(res)
   structure(
     res, class='svg_paths', box=c(x, y, width, height),
     type=type
