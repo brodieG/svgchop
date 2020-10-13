073e1c8213ebcd8d994db60be407f78f2be29ec7
brodieG <brodieG@users.noreply.github.com>
2020-10-12 20:19:53 -0400

    Bézier docs

--- R/bezier.R
+++ R/bezier.R
@@ -1,10 +1,23 @@
 #' Interpolate a Bézier Curve
 #'
+#' For each Bézier curve implied in `coords`, interpolate `steps` segments using
+#' the formula taken from
+#' [Wikipedia](https://en.wikipedia.org/wiki/Bézier_curve):
+#'
+#' (1 - t)^3*P0 + 3*(1 - t)^2*t*P1 + 3*(1 - t)*t^2*P2 + t^3*P3
+#'
+#' where `P0` is the start point, `P1` and `P2` the controls, and `P3` the end
+#' point.  The steps are computed by using evenly spread values of `t` between 0
+#' and 1.
+#'
 #' @export
 #' @param coords "data.frame" or "list" containing "x" and "y" columns
-#'   corresponding to the points in cubic bezier, with `3n` rows, where `n`
-#'   is the number of Bezier curves spliced together.
-#' @param start numeric(2) x and y coordinates for the start point of the bezier
+#'   corresponding to the points in cubic Bézier, with `3n` rows, where `n`
+#'   is the number of Bezier curves spliced together in an SVG path.  The
+#'   first item is the first control point, as the actual start point must be
+#'   retrieved from the prior element and provided to this function via the
+#'   `start` parameter.
+#' @param start numeric(2) x and y coordinates for the start point of the Bézier
 #' @param steps how many steps to use for each curve
 
 bezier_interp2 <- function(coords, start, steps) {
