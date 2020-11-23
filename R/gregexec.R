0b18fb14bf95c9f439e927de8ac535f1f47c3b06
brodieG <brodieG@users.noreply.github.com>
2020-11-23 07:40:14 -0500

    special case gregexec no match case for regmatches

--- R/gregexec.R
+++ R/gregexec.R
@@ -3,6 +3,11 @@
 #' A mocked up version of a global [regexec()].  Only works in PERL mode.  One
 #' day we'll look at submitting this to r-devel.
 #'
+#' When fed to [base::regmatches()], each entry in the result list will either
+#' be 0 long character vector if no-matches, or a vector that has n x m entries
+#' where n is number of match groups + 1.  So each "n" represents one full match
+#' and the matching groups.
+#'
 #' @noRd
 #' @inheritParams base::grep
 
@@ -17,11 +22,15 @@ gregexec <- function(
    )
    lapply(dat,
      function(x) {
-       y <- c(t(cbind(x, attr(x, "capture.start"))))
-       attributes(y) <- attributes(x)
-       attr(y, "match.length") <- c(
-         t(cbind(attr(x, "match.length"), attr(x, "capture.length")))
-       )
+       if(any(x < 0)) y <- x
+       else {
+         # we want to interleave matches with captures
+         y <- c(t(cbind(x, attr(x, "capture.start"))))
+         attributes(y) <- attributes(x)
+         attr(y, "match.length") <- c(
+           t(cbind(attr(x, "match.length"), attr(x, "capture.length")))
+         )
+       }
        y
      }
    )
